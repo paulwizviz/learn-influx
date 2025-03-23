fa03a3c671fd7511a7d69eba506db4904614c505
Paul Sitoh <paulwizviz@gmail.com>
2025-03-23 21:35:01 +0000

    Refactor to focus on influx
    - Rename scripts to disambiguate deployment
    - Reorganise deployments
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- scripts/compose-single-v2.sh
+++ scripts/compose-single-v2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(basename $(realpath .))" != "go-timeseriesdb" ]; then
+if [ "$(basename $(realpath .))" != "learn-influx" ]; then
     echo "You are outside the scope of the project"
     exit 0
 fi
@@ -12,16 +12,16 @@ COMMAND=$1
 
 case $COMMAND in
     "clean")
-        docker compose -f ./deployments/influx2/docker-compose.yaml down
+        docker compose -f ./deployments/compose/single/docker-compose.yaml down
         docker rmi -f ${INFLUX_IMAGE}
         docker rmi -f $(docker images --filter "dangling=true" -q)
         rm -rf ./deployments/influxdb_data
         ;;
     "start")
-        docker compose -f ./deployments/influx2/docker-compose.yaml up
+        docker compose -f ./deployments/compose/single/docker-compose.yaml up
         ;;
     "stop")
-        docker compose -f ./deployments/influx2/docker-compose.yaml down
+        docker compose -f ./deployments/compose/single/docker-compose.yaml down
         ;;
     *)
         echo "Usage: $0 [start | stop]"
